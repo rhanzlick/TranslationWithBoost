3d8254814aeb94945ad9039d1ab9c435f567be88
olekaspt <olekaspt@mail.uc.edu>
2022-02-13 20:02:35 -0500

    Add project files.

---
+++ translation/translation.cpp
@@ -0,0 +1,45 @@
+#include <boost/locale.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace boost::locale;
+
+int main()
+{
+    generator gen;
+
+    // Specify location of dictionaries
+    gen.add_messages_path(".");
+    gen.add_messages_domain("hello");
+
+    // Generate locales and imbue them to iostream
+    locale::global(gen("de_DE.UTF - 8"));
+    cout.imbue(locale());
+
+    // Display a message using current system locale
+    cout << translate("Hello World") << endl;
+
+    cout << translate("file", "open") << endl;
+
+    int files = 1;
+
+    cout << format(translate("You have {1} file in the directory",
+        "You have {1} files in the directory",
+        files)) % files << endl;
+
+   
+    files = 4;
+    cout << format(translate("You have {1} file in the directory",
+        "You have {1} files in the directory",
+        files)) % files << endl;
+
+    int n = 5;
+    cout << format(translate("Option1 {1} file in the directory",
+        "Option2 {1} files in the directory",
+        n)) % n << endl;
+
+    
+
+
+}
+
